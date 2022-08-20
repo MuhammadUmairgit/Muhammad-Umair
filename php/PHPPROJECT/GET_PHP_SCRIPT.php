79fdc69263691f9630f33005b155191a37c201b5
Umair2005 <umairatiq2005@gmail.com>
2022-08-20 03:34:41 -0700

    html-css

--- php/PHPPROJECT/GET_PHP_SCRIPT.php
+++ php/PHPPROJECT/GET_PHP_SCRIPT.php
@@ -1,2 +1,24 @@
-Hi <?php echo $_GET["name"]; ?>
-Your email address is: <?php echo $_GET["email"]; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./style (2).css">
+    <title>Next</title>
+</head>
+<body>
+    <form action="GET_PHP_SCRIPT (2).PHP" method="get">
+        <h2> Hi <?php echo $_GET["name"];?>! </h2><br>
+        <h2>Your email address is: <?php echo $_GET["email"];?> </h2><br>
+        <h2>Your Age is: <?php echo $_GET ["age"];?> </h2><br>
+        <h2> Your Password is: <?php echo $_GET ["password"];?></h2><br>
+        <input type="submit" id="button">
+        
+    </form>
+
+        <script>
+            alert('confirm all');
+        </script>
+</body>
+</html>
--- php/PHPPROJECT/index.php
+++ php/PHPPROJECT/index.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./style.css">
     <title>Document</title>
 </head>
 <body>
         <form action="GET_PHP_SCRIPT.PHP" method="get">
-            Name: <input type="text" name="name"><br>
-            Email: <input type="text" name="email"><br>
-            <input type="submit">
+            <input type="text" name="name" placeholder="Name"><br>
+            <input type="text" name="email" placeholder="Email"><br>
+            <input type="text" name="age" placeholder="Age"><br>
+            <input type="password" name="password" placeholder="password"><br>
+            <input type="submit" id="button">
         </form>
 </body>
 </html>
