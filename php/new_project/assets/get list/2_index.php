3c7c79dd78d425dac23456cbc0dcee05af9726e0
Umair2005 <umairatiq2005@gmail.com>
2022-08-30 09:30:22 -0700

    html-css

---
+++ php/new_project/assets/get list/2_index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/2_style.css">
+    <title>Seccap</title>
+</head>
+<body>
+    <header>
+        <img src="../images/download.png">
+        <nav>
+            <ul>
+                <button class="header_navigation">Home</button>
+                <button class="header_navigation">Admission Policy</button>
+                <button class="header_navigation">Collages</button>
+                <form action="assets/get list/2_index.php" method="get">
+                    <button class="header_navigation" type="submit">Check Status</button>
+                </form>
+                <button class="header_navigation">Cut off</button>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ php/new_project/assets/get list/signin_index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/signin_style.css">
+    <title>Seccap</title>
+</head>
+<body>
+    <h1>SECCAP</h1>
+    <div id="main">
+        <h2>Sign in to your account</h2>
+        <p id="only">Only for Admin</p>
+        <p class="p">Email</p>
+        <input type="text">
+        <p class="p">Password</p>
+        <input type="text">
+        <button type="submit">Continue</button>
+    </div>
+</body>
+</html>
