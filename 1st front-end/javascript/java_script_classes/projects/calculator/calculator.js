fa83f8d0f2de30754416b226f66c0088e025b567
Umair2005 <umairatiq2005@gmail.com>
2022-09-25 11:07:59 +0500

    html-css

---
+++ 1st front-end/javascript/java_script_classes/projects/calculator/calculator.js
@@ -0,0 +1,12 @@
+const allButtons = document.querySelectorAll(".btn");
+
+
+
+
+if() {
+    allButtons.forEach(function (singleButton)) {
+        singleButton.addEventListner("click",function (event) {
+            event.preventDefault();
+        })
+    }
+}
