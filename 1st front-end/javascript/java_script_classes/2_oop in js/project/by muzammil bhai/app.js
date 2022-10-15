3492ce47f66e9b67f6364208ad8ade01d5664918
Umair2005 <umairatiq2005@gmail.com>
2022-10-15 10:55:32 +0500

    html-css

--- 1st front-end/javascript/java_script_classes/2_oop in js/project/by muzammil bhai/app.js
+++ 1st front-end/javascript/java_script_classes/2_oop in js/project/by muzammil bhai/app.js
@@ -4,6 +4,8 @@ const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const isbnInput = document.querySelector("#isbn");
 const bookList = document.querySelector("#book-list");
+const container = document.querySelector(".container");
+const bookForm = document.querySelector("#book-form");
 
 //OOPS stands for object oriented programming
 function UI() {}
@@ -17,6 +19,8 @@ UI.prototype.addBook = function (book) {
           <td>${book.isbn}</td>
           <td><a href="#" class="delete">X<a></td>
         </tr>`;
+
+  // tableRow.querySelector("a").addEventListener("click", bookDeleteHandler);
   bookList.appendChild(tableRow);
 };
 
@@ -24,6 +28,18 @@ UI.prototype.clearFields = function () {
   titleInput.value = "";
   authorInput.value = "";
   isbnInput.value = "";
+};
+
+UI.prototype.showAlert = function (message, className = "success") {
+  const createDiv = document.createElement("div");
+  createDiv.className = `alert ${className} `;
+  createDiv.innerText = message;
+
+  container.insertBefore(createDiv, bookForm);
+
+  setTimeout(function () {
+    createDiv.remove();
+  }, 1000);
 };
 
 function CreateBookObject(title, author, isbn) {
@@ -48,5 +64,32 @@ function formSubmitHandler(event) {
   // console.log(bookObject, "bookObject");
   // ui.addBook({ title: titleValue, author: authorValue, isbn: isbnValue });
   ui.addBook(bookObject);
+  ui.showAlert("book is addedd successfully!");
   ui.clearFields();
+}
+
+// function bookDeleteHandler(event) {
+//   const currentElement = event.target;
+//   const exactTableRow = currentElement.parentElement.parentElement;
+
+//   if (confirm("Are You Sure ?")) {
+//     exactTableRow.remove();
+//   }
+// }
+
+bookList.addEventListener("click", bookListHandler);
+//event bubling when ever you bind the event listener to parent element it will effect all your children element
+function bookListHandler(event) {
+  event.preventDefault();
+  // event.stopPropagation(); // it will prevent the event bubling funcationality
+  const currentElement = event.target;
+  if (currentElement.className == "delete") {
+    const exactTableRow = currentElement.parentElement.parentElement;
+
+    if (confirm("Are You Sure ?")) {
+      exactTableRow.remove();
+      const ui = new UI();
+      ui.showAlert("book is removed successfully!", "error");
+    }
+  }
 }
