a08c1bb15ed6200f8e92d5a2fed3678c844eafb7
Umair2005 <umairatiq2005@gmail.com>
2022-11-29 11:39:01 +0500

    html-css

--- pwa/2_pwa service worker/main.js
+++ pwa/2_pwa service worker/main.js
@@ -1,13 +1,22 @@
+// if (navigator.serviceWorker) {
+//     console.log("service worker is before registered");
+//     navigator.serviceWorker
+//     .register("/sw.js")
+//     .then (function (registration){
+//         registration.onupdatefound = () => {
+//             if (confirm ("App update found. Do you want to update now")) {
+//                 registration?.active?.postMessage("update_self");
+//             }
+//         };
+//     })
+//     .catch(error => console.error (error));
+// }
+
+
+
 if (navigator.serviceWorker) {
-    console.log("service worker is before registered");
     navigator.serviceWorker
     .register("/sw.js")
-    .then (function (registration){
-        registration.onupdatefound = () => {
-            if (confirm ("App update found. Do you want to update now")) {
-                registration?.active?.postMessage("update_self");
-            }
-        };
-    })
-    .catch(error => console.error (error));
+    .then ((registration) => {})
+    .catch(console.error);
 }
