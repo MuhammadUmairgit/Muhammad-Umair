ce6748c6b5d7bcb2b8708793c185c205b9fd5650
Umair2005 <umairatiq2005@gmail.com>
2022-12-03 18:12:32 +0500

    html-css

--- 3-cart-project/sw.js
+++ 3-cart-project/sw.js
@@ -1,36 +1,45 @@
-const CACHE_NAME = `my-sample-app-cache-v1`;
+const pwaCache = "pwa-cache-1";
 
-// Use the install event to pre-cache all initial resources.
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      cache.addAll(["/"]);
-    })()
-  );
+  let cacheReady = caches.open(pwaCache).then((cache) => {
+    return cache.addAll([
+      "/",
+      // "/index.html",
+      // "/style.css",
+      // "/thumb.png",
+      // "/main.js",
+    ]);
+  });
+
+  event.waitUntil(cacheReady);
+});
+
+self.addEventListener("activate", (event) => {
+  let cacheCleaned = caches.keys().then((keys) => {
+    keys.forEach((singleKey) => {
+      if (singleKey !== pwaCache) return caches.delete(key);
+    });
+  });
+
+  event.waitUntil(cacheCleaned);
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
+  if (!event.request.url.match(location.origin)) return;
 
-      try {
-        // Try to fetch the resource from the network.
-        const fetchResponse = await fetch(event.request);
-
-        // Save the resource in the cache.
-        cache.put(event.request, fetchResponse.clone());
-
-        // And return it.
-        return fetchResponse;
-      } catch (e) {
-        // Fetching didn't work get the resource from the cache.
-        const cachedResponse = await cache.match(event.request);
-
-        // And return it.
-        return cachedResponse;
+  let newRes = caches.open(pwaCache).then((cache) => {
+    return cache.match(event.request).then((res) => {
+      if (res) {
+        console.log(`Serving ${res.url} from cache.`);
+        return res;
       }
-    })()
-  );
+
+      return fetch(event.request).then((fetchRes) => {
+        cache.put(event.request, fetchRes.clone());
+        return fetchRes;
+      });
+    });
+  });
+
+  event.respondWith(newRes);
 });
