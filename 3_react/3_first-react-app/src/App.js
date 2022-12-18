4109682464a8b63659889522d098dd552457651d
Muhammad Umair <umairatiq2005@gmail.com>
2022-12-18 13:16:33 +0500

    html-css

--- 3_react/3_first-react-app/src/App.js
+++ 3_react/3_first-react-app/src/App.js
@@ -2,14 +2,27 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, sCount] = useState(0);
+  const [count, setCount] = useState(0);
+  // let count = 0;
 
   const incrementHandler = () => {
-    sCount((zero) => zero + 1);
+    // console.log("clicked");
+    setCount((prevCount) => prevCount + 1);
   };
-}
+  const decrementHandler = () => {
+    const checkDecrementCount = count - 1;
 
-return(
-<button onClick={incrementHandler}>Increment</button>
-)
+    if (checkDecrementCount > 0 || checkDecrementCount === 0) {
+      setCount((prevCount) => prevCount - 1);
+    }
+  };
+  return (
+    <div>
+      <h1>count : {count}</h1>
+      <h2>{count > 0 ? "Value is greater then zero" : "Value is zero"}</h2>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={decrementHandler}>decrement</button>
+    </div>
+  );
+}
 export default App;
