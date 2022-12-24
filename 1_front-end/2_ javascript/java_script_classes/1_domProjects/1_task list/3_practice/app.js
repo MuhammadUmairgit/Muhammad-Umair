d05fec30551fe83272c57fdbdb2b8341e4ec2a3f
Muhammad Umair <umairatiq2005@gmail.com>
2022-12-24 10:55:16 +0500

    doing some work on task list project practice folder

---
+++ 1_front-end/2_ javascript/java_script_classes/1_domProjects/1_task list/3_practice/app.js
@@ -0,0 +1,36 @@
+const taskInput = document.querySelector("#task");
+const taskForm = document.querySelector("#task-form");
+
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const taskInputValue = taskInput.value;
+
+  if (!taskInputValue) {
+    alert("Please Fill The Task!");
+    return;
+  }
+  const listElement = document.createElement("li");
+  listElement.className = "collection-item";
+
+  listElement.innerHTML = `${taskInputValue}
+  <a href="#" class="delete-item secondary-content">
+    <i class="fa fa-remove"></i>
+  </a>`;
+  collection.append(listElement);
+
+  storeTaskInLocalStorage(taskInputValue);
+
+  taskInput.value = "";
+
+  listElement
+    .querySelector(".delete-item i")
+    .addEventListener("click", (event) => {
+      event.preventDefault();
+      const currentElement = event.target;
+      if (confirm("Are You Sure")) {
+        const mainElement = currentElement.parentElement.parentElement;
+        deleteTaskFromLocalStorage(mainElement.innerText);
+        mainElement.remove();
+      }
+    });
+});
