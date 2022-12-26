d6998ce47a370f1ef0c0aea21f7bc43423a36289
Muhammad Umair <umairatiq2005@gmail.com>
2022-12-26 11:34:33 +0500

    working on book crud project

--- 3_react/4_react-testing/src/Components/Login.js
+++
@@ -1,9 +0,0 @@
-const Login = () => {
-    return (
-    <div id="Messsage
-    <div id="Send">
-    <h2> Chat! </h2>
-    <div
-    onClick={() =>
-    auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
-    }
